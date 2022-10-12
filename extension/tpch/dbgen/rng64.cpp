c7eff0a5220dbe9bd9b8744f6dc638222faa69a6
Josh Soref <2119212+jsoref@users.noreply.github.com>
2022-10-12 01:59:38 -0400

    spelling: except
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- extension/tpch/dbgen/rng64.cpp
+++ extension/tpch/dbgen/rng64.cpp
@@ -15,7 +15,7 @@
  * Systems Inc.  By accepting transfer of this copy, Recipient agrees
  * to retain this software in confidence, to prevent disclosure to others, and
  * to make no use of this software other than that for which it was delivered.
- * This is an unpublished copyright work Gradient Systems, Inc.  Execpt as
+ * This is an unpublished copyright work Gradient Systems, Inc.  Except as
  * permitted by federal law, 17 USC 117, copying is strictly prohibited.
  *
  * Gradient Systems Inc. CONFIDENTIAL - (Gradient Systems Inc. Confidential
