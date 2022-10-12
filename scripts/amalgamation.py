b970ce191c2d857799a9585252223629866ed8c4
Josh Soref <2119212+jsoref@users.noreply.github.com>
2022-10-12 04:02:49 -0400

    tolerate no tags

--- scripts/amalgamation.py
+++ scripts/amalgamation.py
@@ -228,8 +228,14 @@ def git_commit_hash():
     return subprocess.check_output(['git','log','-1','--format=%h']).strip().decode('utf8')
 
 def git_dev_version():
-    version = subprocess.check_output(['git','describe','--tags','--abbrev=0']).strip().decode('utf8')
-    long_version = subprocess.check_output(['git','describe','--tags','--long']).strip().decode('utf8')
+    try:
+        version = subprocess.check_output(['git','describe','--tags','--abbrev=0']).strip().decode('utf8')
+    except subprocess.CalledProcessError:
+        version = '0.0.0'
+    try:
+        long_version = subprocess.check_output(['git','describe','--tags','--long']).strip().decode('utf8')
+    except subprocess.CalledProcessError:
+        long_version = version+"-0"
     version_splits = version.split('.')
     dev_version = long_version.split('-')[1]
     if int(dev_version) == 0:
