5b4cf6991419211de6ece90f95aa1e2bc21145e5
Josh Soref <2119212+jsoref@users.noreply.github.com>
2022-10-12 02:00:15 -0400

    spelling: overwrite
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- extension/fts/fts_indexing.cpp
+++ extension/fts/fts_indexing.cpp
@@ -283,7 +283,7 @@ string create_fts_index_query(ClientContext &context, const FunctionParameters &
 	// throw error if an index already exists on this table
 	auto &catalog = Catalog::GetCatalog(context);
 	if (catalog.schemas->GetEntry(context, fts_schema) && !overwrite) {
-		throw CatalogException("a FTS index already exists on table '%s.%s'. Supply 'overwite=1' to overwrite, or "
+		throw CatalogException("a FTS index already exists on table '%s.%s'. Supply 'overwrite=1' to overwrite, or "
 		                       "drop the existing index with 'PRAGMA drop_fts_index()' before creating a new one.",
 		                       qname.schema, qname.name);
 	}
